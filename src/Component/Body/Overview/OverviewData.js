accab6cb6c5e1528093b32c5df9508b5b6a7bcf3
munna3676 <88318740+munna3676@users.noreply.github.com>
2022-03-04 18:54:13 +0600

    first commit

---
+++ src/Component/Body/Overview/OverviewData.js
@@ -0,0 +1,89 @@
+import img1 from "../../Image/Vector1.png"
+import img2 from "../../Image/Vector2.png"
+import img3 from "../../Image/Vector3.png"
+import img4 from "../../Image/Vector4.png"
+
+export const priceData = [
+    {
+        name : "market cap",
+        desc : "",
+        price : "300,000,000",
+        color: "3px solid #FF4003"
+    },
+    {
+        name : "circulating supply",
+        desc : "Total",
+        price : "300,000",
+        color: "3px solid #05B4FF"
+    },
+
+    {
+        name : "Supply",
+        desc : "Total",
+        price : "10,000,000",
+        color: "3px solid #ED2FC3"
+    },
+
+
+    {
+        name : "Bets Price",
+        desc : "Current",
+        price : "5.67",
+        color: "3px solid #05B4FF"
+    },
+
+    {
+        name : "Per Bets",
+        desc : "Backing",
+        price : "60.8",
+        color: "3px solid #ED2FC3"
+    },
+
+    {
+        name : "sBets Price",
+        desc : "Current",
+        price : "6,000",
+        color: "3px solid #FF9A02"
+    },
+
+    {
+        name : "sBets Index",
+        desc : "Current",
+        price : "80.5 sBTS",
+        color: " 3px solid #FF9A02"
+    },
+
+    {
+        name : "sBets Index",
+        desc : "Current",
+        price : "80.5 sBTS",
+        color: "3px solid #ED2FC3"
+    },
+
+]
+
+
+
+
+export const chartData =[
+    {
+        name : "TOTAL VALUE DEPOSITED",
+        price: "$600,000,000",
+       img : img1
+    },
+    {
+        name : "Market Value of Treasury Assets",
+        price: "$600,000,000",
+        img : img2
+    },
+    {
+        name : "SBETS STAKED",
+        price: "$600,000,000",
+        img : img3
+    },
+    {
+        name : "Protocol Owned Liquidity OHM-DAI",
+        price: "99.8%",
+        img : img4
+    }
+]
